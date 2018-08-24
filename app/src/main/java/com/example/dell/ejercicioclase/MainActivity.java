96dd66964209a1e2f4ae2a5ad43b139da4bbac13
Yesica1 <yesi.10.19@hotmail.com>
2018-08-24 11:56:16 -0500

    Nuevas cosas agregadas

---
+++ app/src/main/java/com/example/dell/ejercicioclase/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dell.ejercicioclase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void dialog_fragment(View v){
+        Toast.makeText(this, "Funcionoooooooooooooooooooooooooooooooooooo", Toast.LENGTH_LONG).show();
+    }
+
+    public void openActivity(View v){
+        Intent i = new Intent(this, SecondActivity.class);
+        startActivity(i);
+    }
+
+
+
+}
